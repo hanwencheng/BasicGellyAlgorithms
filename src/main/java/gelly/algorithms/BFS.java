c016a4291cfb73f952725fd2a30a71004912dfe3
Hanwen Cheng <heawen.cheng@gmail.com>
2016-01-18 19:10:32 +0100

    initial singleton

--- src/main/java/gelly/algorithms/BFS.java
+++ src/main/java/gelly/algorithms/BFS.java
@@ -1,5 +1,6 @@
 package gelly.algorithms;
 
+import org.apache.flink.api.java.DataSet;
 import org.apache.flink.graph.Graph;
 import org.apache.flink.graph.Vertex;
 
@@ -9,8 +10,23 @@ import java.util.List;
  * Created by hanwencheng on 1/18/16.
  */
 public class BFS {
+    DataSet nodeList;
+    DataSet edgeList;
+
+    private static BFS instance = null;
+
+    protected BFS(){
+    }
+
+    public static gelly.algorithms.BFS getInstance() {
+        if(instance == null){
+            return new BFS();
+        }
+        return instance;
+    }
 
     public List printVerteces(Graph graph, Vertex startNode){
-
+        nodeList = graph.getVertices();
+        edgeList = graph.getEdges();
     }
 }
