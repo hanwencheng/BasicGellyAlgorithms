1c99efe7420d152ee15531028873ce1da66d8f0b
Hanwen Cheng <heawen.cheng@gmail.com>
2016-01-25 19:31:21 +0100

    add class ValueParameters

---
+++ src/main/java/com/hanwencheng/ValueParameters.java
@@ -0,0 +1,50 @@
+package com.hanwencheng;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by hanwencheng on 1/25/16.
+ */
+public class ValueParameters {
+    private Map<String, Object> schemasMap;
+
+    public ValueParameters() {
+        this.schemasMap = new HashMap<String ,Object>();
+    }
+
+    /**
+     * create certain schema in map
+     * @param name schema name
+     * @param schema schema object to be stored
+     */
+    public void createScheme(String name, Object schema){
+        this.schemasMap.put(name, schema);
+    }
+
+    /**
+     * delete the schema
+     * @param name schema name
+     */
+    public void deleteSchema(String name){
+        this.schemasMap.remove(name);
+    }
+
+    /**
+     * delete the schema only when the object also mapped to the certain value
+     * @param name the name of the schema
+     * @param schema the schema object
+     */
+    public boolean deleteSchema(String name, Object schema ){
+        return this.schemasMap.remove(name, schema);
+    }
+
+    /**
+     * get the schema
+     * @param name schema name
+     * @return the schema object
+     */
+    public Object getSchema(String name){
+        return this.schemasMap.get(name);
+    }
+}
